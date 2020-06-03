7a95eeb72a017c4935a94dc66d2949b6e6fdd666
Jovan Spasojevic <39347729+norephinephrine@users.noreply.github.com>
2020-06-03 02:20:40 +0200

    Made status page

--- project/js/schedule.js
+++ project/js/schedule.js
@@ -8,10 +8,18 @@ $(document).ready(function(){
 
     if(test==null){
         j_obj={
-            training1:[{"day":0,"time":"16.00-18.00","left":5,id:0},{"day":2,"time":"16.00-18.00","left":20,id:1},{"day":4,"time":"10.00-12.00","left":1,id:2},{"day":0,"time":"13.00-15.00","left":0,id:3}],
-            training2:[{"day":1,"time":"13.00-14.00","left":4,id:0},{"day":1,"time":"14.00-16.00","left":1,id:1},{"day":1,"time":"16.00-18.00","left":0,id:2},{"day":5,"time":"11.00-13.00","left":0,id:3}]
+            training1:[{"day":0,"time":"16.00-18.00","left":5,id:0}
+            ,{"day":2,"time":"16.00-18.00","left":20,id:1}
+            ,{"day":4,"time":"10.00-12.00","left":1,id:2}
+            ,{"day":0,"time":"13.00-15.00","left":0,id:3}],
+
+            training2:[{"day":1,"time":"13.00-14.00","left":4,id:0}
+            ,{"day":1,"time":"14.00-16.00","left":1,id:1},
+            {"day":1,"time":"16.00-18.00","left":0,id:2},
+            {"day":5,"time":"11.00-13.00","left":0,id:3}]
         }
         localStorage.setItem("schedule_data",JSON.stringify(j_obj));
+        test=localStorage.getItem("schedule_data");
     }
 
     var data_j=JSON.parse(test);
@@ -19,7 +27,7 @@ $(document).ready(function(){
      var id_sched = urlParams.get('id_sched');
     
     if(id_sched!=null && data_j[id_sched]!=null){
-        $("#training_type").html("Nedelji Raspored "+"<span style='color:red';>"+id_sched+"</span>");
+        $("#training_type").html("Nedeljni Raspored "+"<span style='color:red';>"+id_sched+"</span>");
 
         today=new Date();
         today.setHours(0,0,0,0);
@@ -28,8 +36,10 @@ $(document).ready(function(){
         var day_arr=["PONEDELJAK","UTORAK","SREDA","ČETVRTAK","PETAK","SUBOTA","NEDELJA"];
         for (let i = 0; i < 7; i++) {
             script += "<th>";
-            script+=day_arr[(day+i)%7];
+            script+=day_arr[(day+i)%7]+"<br>"+today.getDate()+"/"+(today.getMonth()+1)+"/"+today.getFullYear();
             script += "</th>";
+            today  = new Date(today.getTime()+86400000);
+
         }
         $("#sched_names").append(script);
 
@@ -54,7 +64,8 @@ $(document).ready(function(){
            }
 
            var disabled="";
-           if(element.left==0)
+           var current_time= new Date($.now());
+           if(element.left==0 || ( element.day==day && (current_time.getHours()>element.time.substring(0,2) || (current_time.getHours()==element.time.substring(0,2) && current_time.getMinutes()==element.time.substring(3,5) ))))
             disabled="disabled";
            list_rows[indexes[cur_day]-1].days[cur_day]="<td>"+"<span>"+element.time+"</span>"+"<h6>"+id_sched+"</h6>"+"<span>"+"Ostalo Mesta:"+element.left+"</span>"+'<button type="button" id='+element.id+" "+disabled+' class="btn btn-primary reserve_button">Rezervacija</button>'+"</td>";
 
@@ -72,4 +83,11 @@ $(document).ready(function(){
         });
 
     }
+    $(".reserve_button").click(function(){
+        
+        var urlParams = new URLSearchParams(window.location.search);
+        var id_sched = urlParams.get('id_sched');        
+        window.open("status_page.html?id="+$(this).attr("id")+"&workout="+id_sched,"_self")
+    })
+
 });
